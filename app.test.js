a1640ff1dcd3c64773d0967c53df7f516f74cabd
miinsoo <ms1226f@gmail.com>
2024-12-06 00:52:04 +0900

    fix3 app.js

--- app.test.js
+++ app.test.js
@@ -14,7 +14,7 @@ describe('HTTP Server', () => {
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.text).to.equal('Hello, World!\n');
+        expect(res.text).to.equal('Hello, World!\n 2171394 채민수의 실습\n');
         done();
       });
   });
