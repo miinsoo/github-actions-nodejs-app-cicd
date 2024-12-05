3e0c28cb1124f2639e47f091acf0de747216ce18
miinsoo <ms1226f@gmail.com>
2024-12-06 00:24:33 +0900

    fix app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n 2171394 채민수\n');
+  res.end('Hello, World!\n 2171394 채민수의 실습\n');
 });
 
 server.listen(port, hostname, () => {
